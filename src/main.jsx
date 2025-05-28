8c9ddbfaf753ac8b3306e790c0c61e673108f8c2
mani <manikarasani.9912@gmail.com>
2025-05-28 15:03:43 -0500

    Deploying portfolio site

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Portfolio from './Portfolio.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Portfolio from './Portfolio.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Portfolio />
   </React.StrictMode>
-);
+)
+
